5518346ab5f63e6f4bdbb40748998456375716e6
whodanyalahmed <daniahmedkhatri@gmail.com>
2024-10-08 22:07:57 +0500

    folder deleted and apostrophe issue fixed

--- src/app/Components/About/ProWebStory.jsx
+++ src/app/Components/About/ProWebStory.jsx
@@ -15,7 +15,7 @@ export default function ProWebStory() {
             {/* Column for Paragraph */}
             <div className="md:w-1/2">
               <p className="mt-4 font-medium text-black">
-                Pro Web is a team of 70+ highly qualified and experienced professionals offering technological solutions by industry standards from the year 2012. We've accomplished business & technology objectives for both medium-sized and larger-scale corporations to meet their demands of long-term growth & success in several domains including Web Design, Web Development, Mobile App Development, E-commerce Development, Web Maintenance, SEO, Google Ads Management, Web Hosting Domain & Dedicated Hiring.
+                Pro Web is a team of 70+ highly qualified and experienced professionals offering technological solutions by industry standards from the year 2012. We&apos;ve accomplished business & technology objectives for both medium-sized and larger-scale corporations to meet their demands of long-term growth & success in several domains including Web Design, Web Development, Mobile App Development, E-commerce Development, Web Maintenance, SEO, Google Ads Management, Web Hosting Domain & Dedicated Hiring.
               </p>
             </div>
           </div>
