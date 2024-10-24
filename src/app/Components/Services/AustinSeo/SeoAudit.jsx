28f5680d57d01d869f9e080fce2975725dc6a827
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2024-10-24 13:37:41 -0700

    vbhh

--- src/app/Components/Services/AustinSeo/SeoAudit.jsx
+++ src/app/Components/Services/AustinSeo/SeoAudit.jsx
@@ -7,7 +7,8 @@ export default function SeoAudit() {
             <section className="bg-black py-12">
                 <div className='container'>
                 <h1 className="text-white md:text-center text-3xl md:text-4xl lg:text-5xl font-bold"> {/* Responsive text size */}
-                    Get Your FREE Instant <span className="text-red-500 font-semibold">SEO Audit Report Now!</span>
+                Get a Free SEO Audit and 
+                <span className="text-red-500 font-semibold"> Boost Your Rankings!</span>
                 </h1>
                 <div className="container mx-auto pt-12 flex flex-col md:flex-row justify-center items-center"> {/* Stack on small screens */}
                     {/* Left Column: Form 3/5 */}
