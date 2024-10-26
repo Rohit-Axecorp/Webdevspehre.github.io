898851b90cea346693f01ac3d5a087af7e3c7304
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2024-10-25 17:14:56 -0700

    more

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -1,23 +1,23 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import HashLoader from 'react-spinners/HashLoader';
-import Header from '@/app/Components/Header'; 
-import Footer from '@/app/Components/Footer'; 
-import Hero from '@/app/Components/Home/Hero'; 
-import Counter from '@/app/Components/Home/Counter'; 
-import SocialIcons from '@/app/Components/Home/SocialIcons'; 
-import Portfolio from '@/app/Components/Home/Portfolio'; 
-import Tabs from '@/app/Components/Home/Tabs'; 
-import Strategies from '@/app/Components/Home/Strategies'; 
-import Publishing from '@/app/Components/Home/Publishing'; 
-import Awards from '@/app/Components/Home/Awards'; 
-import Services from '@/app/Components/Home/Services'; 
-import Testimonials from '@/app/Components/Home/Testmonials'; 
-import ContactForm from '@/app/Components/Home/ContactForm'; 
-import Banner from '@/app/Components/Home/Banner'; 
-import AccordiansFAQ from '@/app/Components/Home/AccordiansFAQ'; 
-import CTAsection from '@/app/Components/Home/CTAsection'; 
-import Process from '@/app/Components/Home/Process'; 
+import Header from '@/app/Components/Header';
+import Footer from '@/app/Components/Footer';
+import Hero from '@/app/Components/Home/Hero';
+import Counter from '@/app/Components/Home/Counter';
+import SocialIcons from '@/app/Components/Home/SocialIcons';
+import Portfolio from '@/app/Components/Home/Portfolio';
+import Tabs from '@/app/Components/Home/Tabs';
+import Strategies from '@/app/Components/Home/Strategies';
+import Publishing from '@/app/Components/Home/Publishing';
+import Awards from '@/app/Components/Home/Awards';
+import Services from '@/app/Components/Home/Services';
+import Testimonials from '@/app/Components/Home/Testmonials';
+import ContactForm from '@/app/Components/Home/ContactForm';
+import Banner from '@/app/Components/Home/Banner';
+import AccordiansFAQ from '@/app/Components/Home/AccordiansFAQ';
+import CTAsection from '@/app/Components/Home/CTAsection';
+import Process from '@/app/Components/Home/Process';
 import Technologies from '@/app/Components/Home/Technologies';
 import Solutions from '@/app/Components/Home/Solutions';
 
@@ -48,7 +48,7 @@ export default function Homepage() {
                     <Tabs />
                     <Strategies />
                     <Publishing />
-                    <Solutions />   
+                    <Solutions />
                     <Services />
                     <Awards />
                     <Technologies />
