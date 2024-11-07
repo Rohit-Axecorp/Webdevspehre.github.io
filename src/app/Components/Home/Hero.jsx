c1c6c3b525449591cefc6a726774834ee2201b2e
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2024-11-06 22:31:41 -0800

    live chaning

--- src/app/Components/Home/Hero.jsx
+++ src/app/Components/Home/Hero.jsx
@@ -16,12 +16,14 @@ export default function Hero() {
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end items-start text-black p-4 md:p-6 lg:p-8">
         <div className="container mx-auto">
           {/* Content Box */}
-          <div className="bg-white bg-opacity-50 p-4 sm:p-6 lg:p-8 rounded-2xl max-w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-3xl">
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 leading-tight">
+
+          <h1 className='text-red-500 text-2xl h1x-4 sm:px-6 lg:px-8 font-semibold'>Web designing and development company USA</h1>
+          <div className=" p-4 sm:p-6 lg:p-8 rounded-2xl max-w-full sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-3xl">
+            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-2 leading-tight text-white">
               Digitizing Business Experience with for Sustainable Growth
 
-            </h1>
-            <p className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl my-4 leading-relaxed font-normal">
+            </p>
+            <p className="text-base sm:text-lg md:text-xl lg:text-xl xl:text-2xl my-4 leading-relaxed font-normal text-white">
               Sell experience rather than a product or service. WebDev Sphere has introduced a way for startups to create an impact on clients with its futuristic web design and development services. With user-centric UI to highly functional features and mobile-first experience, we create websites from scratch right according to your brand purpose. Plus, you can sustain your business online by leveraging our secret digital marketing strategy.
             </p>
             <button className="group relative px-4 py-2 sm:px-5 sm:py-3 lg:px-6 lg:py-4 border border-black rounded-full text-sm sm:text-lg flex items-center justify-center gap-2 transition-all duration-300 hover:bg-[#ED1E3A] hover:text-white hover:border-transparent">
