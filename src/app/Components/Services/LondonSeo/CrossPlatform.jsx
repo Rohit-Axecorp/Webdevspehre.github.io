a733da3afdc93590b610daa26675a312676c6916
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2024-12-09 21:56:38 -0800

    london page is done

---
+++ src/app/Components/Services/LondonSeo/CrossPlatform.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function CrossPlatform() {
+    return (
+        <>
+            <section className='container 2xl:py-12 py-10 px-4 sm:px-6 lg:px-8'>
+                <h4 className="text-[#ED1E3A]xl lg:text-4xl 2xl:text-5xl font-bold text-center mb-8"><span className='text-[#ED1E3A]'> Why Choose Web Dev Sphere
+                </span>
+                    <br />As Your Cross-Platform App Development Company?
+                </h4>
+                <div className="flex flex-col lg:flex-row justify-between items-center md:items-start ">
+                    {/* Left Column - Code Section */}
+                    <div className="lg:w-1/2 bg-white lg:pr-8 mb-8 lg:mb-0">
+
+                        <p className="mt-6 text-black 2xl:text-lg lg:text-base text-sm">
+                            Web Dev Sphere knows cross-platform app development at its core. Our passion for learning and adapting to the latest tech gives us an edge over our competition.
+                        </p>
+                        <p className="mt-6 text-black 2xl:text-lg lg:text-base text-sm">
+                            We harness the power of the latest technologies for cross-platform app development and deliver apps that run smoothly on various platforms.
+                        </p>
+                        <p className="mt-6 text-black 2xl:text-lg lg:text-base text-sm">
+                            Web Dev Sphere's cross-platform prowess guarantees cost-effectiveness, rapid deployment, and a strategic edge in the ever-evolving market, marking a seamless journey toward app excellence and market dominance.
+                        </p>
+                        <p className="mt-6 text-black 2xl:text-lg lg:text-base text-sm">
+                            Choosing Web Dev Sphere for your cross platform mobile application development guarantees a myriad of benefits derived from our specialized proficiency and unwavering dedication to achieving excellence.
+                        </p>
+                        <button className="group mt-5 relative px-6 py-3 text-white border-[#ED1E3A] bg-red-600 rounded-full 2xl:text-2xl lg:text-lg text-base  flex items-center justify-center gap-2 transition-all duration-300 hover:bg-red-600 hover:text-white hover:border-transparent">
+                            Schedule A Free Consultation Session
+                        </button>
+                    </div>
+
+                    <div className="w-full lg:w-1/2">
+                        <img
+                            src="/Images/Why-Choose.webp"
+                            alt="Process Image"
+                            className="rounded-lg md:w-4/5 w-full"
+                        />
+                    </div>
+                </div>
+
+            </section>
+        </>
+    )
+}
--- src/app/london-seo-company/page.js
+++ src/app/london-seo-company/page.js
@@ -1,21 +1,14 @@
 import Footer from '@/app/Components/Footer';
 import Header from '@/app/Components/Header';
 import CTAsection from '@/app/Components/Home/CTAsection';
-import Roadmap from '@/app/Components/Services/LondonSeo/Roadmap';
 import SeoAudit from '@/app/Components/Services/LondonSeo/SeoAudit';
-import SeoBusinessSolutions from '@/app/Components/Services/LondonSeo/SeoBusinessSolutions';
 import SeoCaseStudies from '@/app/Components/Services/LondonSeo/SeoCaseStudies';
-import SeoCompany from '@/app/Components/Services/LondonSeo/SeoCompany';
 import SeoContact from '@/app/Components/Services/LondonSeo/SeoContact';
-import SeoCTA from '@/app/Components/Services/LondonSeo/SeoCTA';
-import SeoLocation from '@/app/Components/Services/LondonSeo/SeoLocation';
 import SeoPackages from '@/app/Components/Services/LondonSeo/SeoPackages';
 import SeoQuote from '@/app/Components/Services/LondonSeo/SeoQuote';
 import SeoRecognition from '@/app/Components/Services/LondonSeo/SeoRecognition';
 import SeoReviews from '@/app/Components/Services/LondonSeo/SeoReviews';
-import SeoServices from '@/app/Components/Services/LondonSeo/SeoServices';
 import SeoServicesIncludes from '@/app/Components/Services/LondonSeo/SeoServicesIncludes';
-import SeoSolution from '@/app/Components/Services/LondonSeo/SeoSolution';
 import TrustedBrands from '@/app/Components/Services/LondonSeo/TrustedBrands';
 import React from 'react';
 import AccordiansFAQ from '@/app/Components/Services/LondonSeo/AccordiansFAQ';
@@ -23,8 +16,7 @@ import RootLayout from '../layout';
 import Healthcare from '../Components/Services/LondonSeo/Healthcare';
 import ServicesLondon from '../Components/Services/LondonSeo/ServicesLondon';
 import SeoLondonTimeline from '../Components/Services/LondonSeo/SeoLondonTimeline';
-
-
+import CrossPlatform from '../Components/Services/LondonSeo/CrossPlatform';
 
 export const metadata = {
     title: "Leading Austin SEO Company | Web Dev Sphere", // Dynamic title
@@ -49,6 +41,7 @@ export default function Page() {
             <SeoPackages />
             <SeoLondonTimeline />
             <SeoRecognition />
+            <CrossPlatform />
             <SeoQuote />
             <AccordiansFAQ />
             <CTAsection />
