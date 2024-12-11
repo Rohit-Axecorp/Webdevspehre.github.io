4a78479f2283e15dbb4b4e16054a8d944a47f137
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2024-12-11 23:48:57 +0500

    changing

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children, canonicalUrl }) {
   const pathname = usePathname(); // Get the current path
 
   return (
-    <html lang="en-us">
+    <html lang="en-US">
       <head>
         {/* Always Include Global Meta Tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,24 +27,24 @@ export default function RootLayout({ children, canonicalUrl }) {
               property="og:description"
               content="Leading web development and design agency delivering affordable modern and responsive web design services in Houston TX USA."
             />
-            <meta property="og:image" content="https://webdevsphere.com/logo.png" />
+            {/* <meta property="og:image" content="https://webdevsphere.com/logo.png" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@webdevsphere" />
             <meta name="twitter:title" content="Web Dev Sphere" />
             <meta name="twitter:description" content="Leading web development and design agency delivering affordable modern and responsive web design services in Houston TX USA." />
-            <meta name="twitter:image" content="https://webdevsphere.com/logo.png" />
+            <meta name="twitter:image" content="https://webdevsphere.com/logo.png" /> */}
             <link rel="canonical" href="https://webdevsphere.com" />
-            <link rel="alternate" hrefLang="en-US" href="https://webdevsphere.com" />
-            <link rel="alternate" hrefLang="x-default" href="https://webdevsphere.com" />
+            {/* <link rel="alternate" hrefLang="en-US" href="https://webdevsphere.com" />
+            <link rel="alternate" hrefLang="x-default" href="https://webdevsphere.com" /> */}
             <link rel="me" href="https://www.facebook.com/webdevsphere1" />
             <link rel="me" href="https://www.instagram.com/webdevsphere" />
             <link rel="me" href="https://www.linkedin.com/company/web-dev-sphere" />
             <link rel="me" href="https://www.pinterest.com/webdevsphere1/" />
             <link rel="me" href="https://www.youtube.com/@webdevsphere1" />
-            <meta name="google-site-verification" content="T5IIkWnlq8DjeYbqup_Ud0biM1DjbK8xh08AwI7UDgM" />
+            {/* <meta name="google-site-verification" content="T5IIkWnlq8DjeYbqup_Ud0biM1DjbK8xh08AwI7UDgM" />
             <meta name="google-site-verification" content="rneVRzNxeftgc8icex1C-tgULkheYe98qU8A8AKy2yo" />
             <meta name="google-site-verification" content="8c3gU25bOefRDZOX84FW6hZati8pKHpSgdyDJwqvw_A" />
-            <meta name="google-site-verification" content="H_aKn9-DZgHX9wAiVAOBKtWuW4YeByxHq3mE0RQl8X4" />
+            <meta name="google-site-verification" content="H_aKn9-DZgHX9wAiVAOBKtWuW4YeByxHq3mE0RQl8X4" /> */}
 
             {/* JSON-LD for Organization */}
             <script
