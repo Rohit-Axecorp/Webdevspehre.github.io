bb182b631394cc954c5320497473922d1c4ccb46
Rohit-Axecorp <singhaniarohitram77@gmail.com>
2025-03-18 20:15:21 +0500

    blogs removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         unoptimized: true, // Disable image optimization
     },
-    output: 'export', // Set the output mode to export static files
     distDir: 'out',   // Set the output directory to 'out' (default is '.next')
 };
 
